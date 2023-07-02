e8140b2df53fcb1d19905cff2df0e2b5eaf9a41c
Yulchikss <yuliapelia19@gmail.com>
2023-07-02 17:03:25 +0300

    feat: create media css

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const menu = document.querySelector(".header_menu"),
-    hamburger = document.querySelector(".hamburger");
+  const menu = document.querySelector(".header_menu");
+  const hamburger = document.querySelector(".hamburger");
 
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("hamburger_active");
     menu.classList.toggle("menu_active");
   });
-
-  // menuItem.forEach((item) => {
-  //   item.addEventListener("click", () => {
-  //     hamburger.classList.toggle("hamburger_active");
-  //     menu.classList.toggle("menu_active");
-  //   });
-  // });
 });
